617d92e9e335b44e22aa8d0e40de56697bf8ff9c
zvy26 <komacoder26@gmail.com>
2025-09-08 17:50:03 +0500

    some changes...

--- src/pages/ManageInterests.tsx
+++ src/pages/ManageInterests.tsx
@@ -254,7 +254,7 @@ export const ManageInterests: React.FC = () => {
                   value={addData.name}
                   onChange={(e) => setAddData(prev => ({ ...prev, name: e.target.value }))}
                   className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  placeholder="nter name"
+                  placeholder="Enter name"
                   required
                 />
               </div>
